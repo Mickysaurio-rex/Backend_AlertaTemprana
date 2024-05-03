ea840643c8841445ccfe3883bf33bc856f4d96d3
micky <mickymolina@gmail.com>
2024-05-02 20:37:09 -0400

    Carreando

--- src/modulos/usuarios/controlador.js
+++ src/modulos/usuarios/controlador.js
@@ -33,11 +33,11 @@ module.exports = function (dbinyectada){
         }
 
         let respuesta2 = '';
-        if(body.usuario && body.password){
+        if(body.correo || body.contrsena){
                respuesta2 = await auth.agregar({
                 id: insertID,
-                correo: body.usuario,
-                contrasena: body.password
+                correo: body.correo,
+                contrasena: body.contrasena
 
             })
         }
