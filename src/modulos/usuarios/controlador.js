c0655a8cef61d2b942d18b240012f567480800c0
micky <mickymolina@gmail.com>
2024-05-02 20:27:29 -0400

    Carreando

--- src/modulos/usuarios/controlador.js
+++ src/modulos/usuarios/controlador.js
@@ -36,8 +36,8 @@ module.exports = function (dbinyectada){
         if(body.usuario && body.password){
                respuesta2 = await auth.agregar({
                 id: insertID,
-                correo: body.usuario,
-                contrasena: body.password
+                usuario: body.usuario,
+                password: body.password
 
             })
         }
