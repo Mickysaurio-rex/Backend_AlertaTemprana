c6ae5a92db142d9c0f350cb0fe215c80607a0ee0
Gabriel Neme <gaboneme11@gmail.com>
2024-05-05 22:56:25 -0400

    Adding testing branch and some test for the routes

---
+++ src/test/app.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const app = require('../app'); 
+
+describe('Pruebas de la API', () => {
+  test('Debería responder con éxito a una solicitud GET a /api/usuarios', async () => {
+    const response = await request(app).get('/api/usuarios');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/alertas', async () => {
+    const response = await request(app).get('/api/alertas');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/estado', async () => {
+    const response = await request(app).get('/api/estado');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/tipo_alerta', async () => {
+    const response = await request(app).get('/api/tipo_alerta');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/tipo_desastre', async () => {
+    const response = await request(app).get('/api/tipo_desastre');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/zona', async () => {
+    const response = await request(app).get('/api/zona');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Debería responder con éxito a una solicitud GET a /api/numeros_emergencia', async () => {
+    const response = await request(app).get('/api/numeros_emergencia');
+    expect(response.statusCode).toBe(200);
+  });
+});
